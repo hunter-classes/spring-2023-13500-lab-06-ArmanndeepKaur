ad671495fcc6488723720b49b6bfa62c27d35360
ArmanndeepKaur <armanndeep.22@gmail.com>
2023-03-10 07:00:53 -0500

    edited

--- main.cpp
+++ main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
-
-
-#include "funcs.h"
+#include <string>
+#include <cctype>
+#include "caesar.h"
+#include "vigenere.h"
+#include "decrypt.h"
 
 int main()
 {
+
+  //TASK B 
+  std::cout<<encryptCaesar("Hello, World", 10)<<"\n";
+  std::cout<<encryptCaesar("Hi, Everyone", 15)<<"\n";
+
+  //Task C
+  std::cout<<encryptVigenere("Hello, World!", "cake")<<"\n";
+  std::cout<<encryptVigenere("Bye, World!", "make")<<"\n";
+
+  //Task D
+  std::cout<<decryptCaesar("Rovvy, Gybvn!", 10)<<"\n";
+  std::cout<<decryptVigenere("Jevpq, Wyvnd!","cake")<<"\n";
+
   return 0;
 }
